d0734fa36a97ad7ccd79fd2d804348a2b1a35908
root <root@javacst001.localdomain>
2014-10-19 18:59:15 +0900

    update

--- programming-in-scala/checksum_accumulator.scala
+++ programming-in-scala/checksum_accumulator.scala
@@ -4,3 +4,20 @@ class ChecksumAccumulator {
 	def checksum(): Int = -(sum & 0xFF) + 1
 }
 
+import scala.collection.mutable.Map
+
+object ChecksumAccumulator {
+	private var cache = Map[String, Int]()
+	def calculate(s: String): Int =
+		if(cache.contains(s))
+			cache(s)
+		else {
+			val acc = new ChecksumAccumulator
+			for (c <- s)
+				acc.add(c.toByte)
+			val cs = acc.checksum()
+			cache += (s -> cs)
+			cs
+		}
+}
+
